bc7dc39144dc634aa17601057c3ba4bcf54eee3e
Muhammedko90 <kocm942@gmail.com>
2025-11-29 20:39:51 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'gezgin-cache-v1';
+const urlsToCache = [
+  './Gezgin.html',
+  './manifest.json',
+  './icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
